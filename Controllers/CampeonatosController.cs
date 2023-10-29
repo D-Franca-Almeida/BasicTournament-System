a886e8d783b229fc568d47a176594bda1e3436d5
Daniel França Almeida <daniel.franca0512@gmail.com>
2023-10-29 20:09:19 -0300

    Adicionando Campeonato, Model View e Controller

---
+++ Controllers/CampeonatosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using BasicTournament_System.Models;
+
+namespace BasicTournament_System.Controllers
+{
+    public class CampeonatosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Campeonato> list = new List<Campeonato>();
+            list.Add(new Campeonato
+            {
+                Id = 1,
+                Titulo = "Luta Livre",
+                Imagem = "PathEmpty",
+                Cidade = "Votorantim",
+                Estado = "São Paulo",
+                DataRealizacao = DateTime.Now,
+                Dia = DateTime.Now.Day,
+                Mes = DateTime.Now.Month,
+                Ano = DateTime.Now.Year,
+                SobreEvento = "Evento de lutas marciais",
+                Ginasio = "Estádio Poliesportivo",
+                InformacoesGerais = "Varias categorias",
+                EntradaPublico = "Gratuita",
+                Tipo = "Kimono",
+                Fase = 1,
+                Status = 1
+            });
+            list.Add(new Campeonato
+            {
+                Id = 2,
+                Titulo = "Corrida",
+                Imagem = "PathEmpty",
+                Cidade = "Votorantim",
+                Estado = "São Paulo",
+                DataRealizacao = DateTime.Now,
+                Dia = DateTime.Now.Day,
+                Mes = DateTime.Now.Month,
+                Ano = DateTime.Now.Year,
+                SobreEvento = "Evento de corrida",
+                Ginasio = "Estádio Poliesportivo",
+                InformacoesGerais = "Maratona 20km",
+                EntradaPublico = "Gratuita",
+                Tipo = "Confortável",
+                Fase = 1,
+                Status = 1
+            });
+            return View(list);
+        }
+    }
+}
---
+++ Models/Campeonato.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicTournament_System.Models
+{
+    public class Campeonato
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Imagem { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public DateTime DataRealizacao { get; set; }
+        public int Dia {  get; set; }
+        public int Mes {  get; set; }
+        public int Ano { get; set; }
+        public string SobreEvento { get; set; }
+        public string Ginasio { get; set; }
+        public string InformacoesGerais { get; set; }
+        public string EntradaPublico { get; set; }
+        public string Tipo { get; set; } // Kimono ou No-GI
+        public int Fase { get; set; } // Inscrições Abertas(1), Chaves de Lutas(2), Resultados(3)
+        public int Status { get; set; } // Ativo(1) ou Inativo(0)
+    }
+}
